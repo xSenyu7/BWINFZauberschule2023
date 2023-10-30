56796149a1012bc93a183ab731d7238bc51956fd
Fahrenbruch <heinrich.fahrenbruch@adesso.de>
2023-10-30 08:28:04 +0100

    Lauf Algotrhytmus angefangen

--- Zauberschule/Logic/LaufAlgorhytmus.cs
+++ Zauberschule/Logic/LaufAlgorhytmus.cs
@@ -1,15 +1,28 @@
 ﻿
+using Zauberschule.Data;
+
 namespace Zauberschule.Logic
 {
     public class LaufAlgorhytmus
     {
+        int[] position = new int[2];
+
         public void SchnellstenWegFinden()
         {
 
         }
-        private void EtageHoch()
+        private void EtageHoch(Schule schule)
         {
-
+            for (int i = 0; i < position.Length; i++)
+            {
+                for(int j = 0; j < schule.ErsteEtage.Grundriss.Length; i++)
+                {
+                    if (schule.ErsteEtage.Grundriss[i,j] == 'A')
+                    {
+                        position[] = 
+                    }
+                }
+            }
         }
         private void EtageRunter()
         {
