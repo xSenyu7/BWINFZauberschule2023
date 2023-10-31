f13900528fd2ac020b4bfecc27a1722ddf3e8554
Fahrenbruch <heinrich.fahrenbruch@adesso.de>
2023-10-31 09:33:04 +0100

    FloodFill begonnen

--- Zauberschule/Logic/FloodFill.cs
+++ Zauberschule/Logic/FloodFill.cs
@@ -1,11 +1,37 @@
 ﻿
 
+using Zauberschule.Data;
+
 namespace Zauberschule.Logic
 {
     public class FloodFill
     {
 
+        List<Ziel> Zwischenpunkte;
+
+        public void AuffüllenDesStockwerks(Schule schule, Ziel zielpunkt)
+        {
+            string[,] ersteEtage = schule.ErsteEtage.Grundriss;
+
+            while (NotwendigkeitFürAuffüllungPrüfen(ersteEtage) == true)
+            {
+
+            }
+        }
 
 
+
+        public bool NotwendigkeitFürAuffüllungPrüfen(string[,] etage)
+        {
+            foreach (Ziel z in Zwischenpunkte)
+            {
+                if(etage[z.PositionX + 1, z.PositionY] == "."
+                || etage[z.PositionX - 1, z.PositionY] == "."
+                || etage[z.PositionX, z.PositionY + 1] == "."
+                || etage[z.PositionX, z.PositionY - 1] == ".")
+                    return true;
+            }
+            return false;
+        }
     }
 }
