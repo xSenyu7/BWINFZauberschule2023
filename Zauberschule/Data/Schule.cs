1ee3e27a82011bcef6b473edf635d46c0c6f3d0f
Fahrenbruch <heinrich.fahrenbruch@adesso.de>
2023-11-07 15:04:26 +0100

    Sinnlosen Kommentar gelöscht

--- Zauberschule/Data/Schule.cs
+++ Zauberschule/Data/Schule.cs
@@ -11,8 +11,6 @@ namespace Zauberschule.Data
 
         public Stockwerk ErsteEtage { get; set; }
         public Stockwerk ZweiteEtage { get; set; }
-        //public Person Person { get; set; }
-
 
         public Schule(string path)
         {
