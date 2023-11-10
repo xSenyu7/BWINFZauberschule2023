22be7a699389bf80258ccc50c8c83e68f731a9de
Fahrenbruch <heinrich.fahrenbruch@adesso.de>
2023-11-10 09:59:50 +0100

    Enum Etage erstellt

---
+++ Zauberschule/Data/Etage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zauberschule.Data
+{
+    public enum Etage
+    {
+        Oben,
+        Unten
+    }
+}
