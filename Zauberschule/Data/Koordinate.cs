635a2bb654471fa4cb6b22011e69d1ad5b45cda5
Fahrenbruch <heinrich.fahrenbruch@adesso.de>
2023-11-15 12:40:12 +0100

    Vorbereitung für merge in main

---
+++ Zauberschule/Data/Koordinate.cs
@@ -0,0 +1,19 @@
+﻿using Zauberschule.Logic;
+
+namespace Zauberschule.Data
+{
+    public class Koordinate
+    {
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
+
+        public InitialisiereZiel initialisiereZiel;
+
+        public Koordinate(int positionX, int positionY)
+        {
+            PositionX = positionX;
+            PositionY = positionY;
+        }
+
+    }
+}
